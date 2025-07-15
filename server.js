d46473313053bc2897440011787594725ed0da00
parween92 <parweena46@gmail.com>
2025-07-15 10:37:02 +0200

    lokal test

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 8000;
 // CORS für Cookies
 app.use(
   cors({
-    origin: "https://final-project-frontend-twpn.onrender.com",
+    origin: "http://localhost:5173/",
     credentials: true,
   })
 );
