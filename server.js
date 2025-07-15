abe331016fea687992531796ef097b855d466837
parween92 <parweena46@gmail.com>
2025-07-15 10:40:51 +0200

    lokal test

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 8000;
 // CORS für Cookies
 app.use(
   cors({
-    origin: "http://localhost:5173/",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
